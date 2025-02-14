a5cdd36079cabdd16a087c5047c09eea4cbec3a3
elijah-digiteer <johnelijah@digiteer.digital>
2025-02-14 14:27:24 +0800

    init commit

--- config/routes/site.rb
+++ config/routes/site.rb
@@ -1,6 +1,19 @@
-namespace :site do
-    root to: 'home#index'
-    resources :posts
-    resources :users, only: [:show, :edit, :update]
-  end
-  
+devise_for :users, 
+  path: "", 
+  controllers: { 
+    sessions: "site/sessions", 
+    registrations: "site/registrations", 
+    passwords: "site/passwords" 
+  },
+  path_names: {
+    sign_in: "/login",
+    password: "/forgot",
+    confirmation: "/confirm",
+    unlock: "/unblock",
+    registration: "/reserve",
+    sign_out: "/logout",
+    password_expired: "/password-expired"
+  }
+namespace :site, path: "/" do
+  # Routes for the normal users
+end
